64151fe42aaa31875a7a261b0327bee5f829415c
aaron <aaroncvaldez@gmail.com>
2012-08-09 02:21:32 -0500

    metodos index y show en controladora

--- flickr_Aaron/app/controllers/photos_controller.rb
+++ flickr_Aaron/app/controllers/photos_controller.rb
@@ -1,2 +1,17 @@
 class PhotosController < ApplicationController
+require 'flickraw'
+
+def index
+render
+end
+
+def show
+FlickRaw.api_key="681b7acb1c6aacdb4897f2bf1712028f"
+FlickRaw.shared_secret="b14ac2fd13f84040"
+url=params[:url]
+numero=params[:numero]
+@photos = flickr.photos.search(:tags =>url, :per_page => numero)
+
+end
+
 end
