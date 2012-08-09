ee6fe5d32da6ea230dc1ac04a2a25f9426fa5172
aaron <aaroncvaldez@gmail.com>
2012-08-09 02:05:29 -0500

    Se genera new proyect flickr

---
+++ flickr_Aaron/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlickrAaron::Application.config.secret_token = '5de354b5482e6bea8953fc4cb6a76c631403e35fb822437acc432b75cad3785d0c8f327c85ada19265b4058fd6bec414ad95c8b09f9783c5abc863e8779057ae'
